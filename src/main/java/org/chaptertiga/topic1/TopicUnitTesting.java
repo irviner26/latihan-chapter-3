84660f4e370d135c6ba4308ade1be0c96c951ce2
Irvine <irvineringgo90@gmail.com>
2023-09-13 20:41:59 +0700

    Latihan Unit Test BEJ1

--- src/main/java/org/chaptertiga/topic1/TopicUnitTesting.java
+++ src/main/java/org/chaptertiga/topic1/TopicUnitTesting.java
@@ -5,21 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TopicUnitTesting {
-    public static void main(String[] args) {
-        List<Integer> nilaiKelasA;
-        Integer[] arrayA = {90, 85, 89, 77, 60, 88, 80, 90, 45, 70};
-        nilaiKelasA = Arrays.asList(arrayA);
-
-        List<Integer> nilaiKelasB;
-        Integer[] arrayB = {80, 85, 88, 55, 40};
-        nilaiKelasB = Arrays.asList(arrayB);
-
-        List<Integer> nilaiKelasC;
-        Integer[] arrayC = {85, 50, 95, 35, 15};
-        nilaiKelasC = Arrays.asList(arrayC);
-
-    }
-
     public Integer gaLulus(List<Integer> listNilai) {
         Integer jumlahGaLulus = 0;
         for (Integer i : listNilai) {
